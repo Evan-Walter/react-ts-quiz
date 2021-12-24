f613b7eff6772e2db14207c79029185cd135e3a6
LinKzorD <pedro.novo.93@gmail.com>
2021-12-24 12:52:54 +0000

    structuring

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,58 @@
-import React from "react";
+import React, { useState, useEffect, useMemo, useCallback } from "react";
+import axios from "axios";
+import { quizQuestions } from "./quizQuestions";
 import "./App.css";
 
+// type QuestionArr = {
+//   category: string;
+//   type: string;
+//   difficulty: string;
+//   question: string;
+//   correct_answer: string;
+//   incorrect_answers: string[];
+// };
+
 function App() {
+  const [step, stepSet] = useState<number>(-1);
+  const [score, scoreSet] = useState<number>(0);
+  const [gameOver, gameOverSet] = useState<boolean>(true);
+  const [gameQuestions, gameQuestionsSet] = useState(quizQuestions);
+
+  const possibleAnswers = () => {
+    if (step > -1) {
+      return [
+        ...gameQuestions[step].incorrect_answers,
+        gameQuestions[step].correct_answer,
+      ];
+    }
+  };
+
+  if (step === -1) {
+    return (
+      <div>
+        <button type='button' onClick={() => stepSet(step + 1)}>
+          Play Quiz
+        </button>
+      </div>
+    );
+  }
+
   return (
     <div className='App'>
-      <h1>Quiz</h1>
+      <div>
+        <h1>The Easiest Quiz Ever</h1>
+      </div>
+      <div>category: {gameQuestions[step].category} </div>
+      <div>difficulty: {gameQuestions[step].difficulty} </div>
+      <div>question: {gameQuestions[step].question} </div>
+      <div>Category: {gameQuestions[step].category} </div>
+      <div>
+        {possibleAnswers.map((answer: string, index: number) => {
+          <button type='button' key={index}>
+            {answer}
+          </button>;
+        })}
+      </div>
     </div>
   );
 }
---
+++ src/quizQuestions.ts
@@ -0,0 +1,102 @@
+export const quizQuestions = [
+  {
+    category: "Science: Computers",
+    type: "multiple",
+    difficulty: "medium",
+    question: "On which computer hardware device is the BIOS chip located?",
+    correct_answer: "Motherboard",
+    incorrect_answers: [
+      "Hard Disk Drive",
+      "Central Processing Unit",
+      "Graphics Processing Unit",
+    ],
+  },
+  {
+    category: "General Knowledge",
+    type: "multiple",
+    difficulty: "medium",
+    question: "On average, Americans consume 100 pounds of what per second?",
+    correct_answer: "Chocolate",
+    incorrect_answers: ["Potatoes", "Donuts", "Cocaine"],
+  },
+  {
+    category: "Entertainment: Video Games",
+    type: "multiple",
+    difficulty: "easy",
+    question: "In Dota 2, Wraith King was previously known as...",
+    correct_answer: "Skeleton King",
+    incorrect_answers: ["Reaper King", "Skull King", "Hell King"],
+  },
+  {
+    category: "Entertainment: Video Games",
+    type: "multiple",
+    difficulty: "hard",
+    question:
+      "In Monster Hunter Generations, which of these hunter arts are exclusive to the Longsword?",
+    correct_answer: "Unhinged Spirit",
+    incorrect_answers: ["Shoryugeki", "Provoke", "Demon Riot"],
+  },
+  {
+    category: "Entertainment: Board Games",
+    type: "multiple",
+    difficulty: "hard",
+    question:
+      "What do you declare in R\u012bchi Mahjong when you&#039;ve drawn your winning tile?",
+    correct_answer: "Tsumo",
+    incorrect_answers: ["Ron", "R\u012bchi", "Kan"],
+  },
+  {
+    category: "Entertainment: Television",
+    type: "multiple",
+    difficulty: "easy",
+    question:
+      "Who played the Waitress in the Spam sketch of &quot;Monty Python&#039;s Flying Circus&quot;?",
+    correct_answer: "Terry Jones",
+    incorrect_answers: ["Eric Idle", "Graham Chapman", "John Cleese"],
+  },
+  {
+    category: "Animals",
+    type: "multiple",
+    difficulty: "medium",
+    question: "The dish Fugu, is made from what family of fish?",
+    correct_answer: "Pufferfish",
+    incorrect_answers: ["Bass", "Salmon", "Mackerel"],
+  },
+  {
+    category: "Entertainment: Japanese Anime & Manga",
+    type: "multiple",
+    difficulty: "medium",
+    question: "In Haikyuu!!, who is not a member of Karasuno VBC?",
+    correct_answer: "Shigeru Yahaba",
+    incorrect_answers: [
+      "Tadashi Yamaguchi",
+      "Hisashi Kinoshita",
+      "Kazuhito Narita",
+    ],
+  },
+  {
+    category: "Mythology",
+    type: "multiple",
+    difficulty: "easy",
+    question: "What mythology did the god &quot;Apollo&quot; come from?",
+    correct_answer: "Greek and Roman",
+    incorrect_answers: [
+      "Roman and Spanish",
+      "Greek and Chinese",
+      "Greek, Roman and Norse",
+    ],
+  },
+  {
+    category: "Entertainment: Japanese Anime & Manga",
+    type: "multiple",
+    difficulty: "medium",
+    question:
+      "Which of the following films was NOT directed by Hayao Miyazaki?",
+    correct_answer: "Wolf Children",
+    incorrect_answers: [
+      "Princess Mononoke",
+      "Spirited Away",
+      "Kiki&#039;s Delivery Service",
+    ],
+  },
+];
